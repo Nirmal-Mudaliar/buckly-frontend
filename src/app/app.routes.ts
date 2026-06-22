4483e07cb804e821f28caecda0beb13af6404820
Nirmal-Mudaliar <nirmalmudaliar22@gmail.com>
2026-06-23 00:23:58 +0530

    feat: add logic to create login and sign up component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('../app/features/auth/login/login').then(m => m.Login),
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('../app/features/auth/signup/signup').then(m => m.Signup),
+  }
+];
